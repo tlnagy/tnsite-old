9623e6476360e4b82b5855beff516652e4aacc71
Tamas Nagy <tamas@tamasnagy.me>
2014-04-06 16:08:07 -0400

    Fixed s3cmd breakage under Python3
    This is kind of hackish, fix it.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,11 @@
 
 jekyll build
 
+echo "Switching to python2 so s3cmd doesn't break."
+source activate website
+
 echo "Deploying changes..."
 
 s3cmd sync --exclude '.DS_Store' --exclude '.git/*' --exclude-from '.gitignore' --delete-removed --acl-public _site/ s3://www.tamasnagy.com
+
+source deactivate
